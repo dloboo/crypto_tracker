440c070cd29f9d0783001be9cafb6dd064f43095
alumno <alumno@gmail.com>
2025-01-16 17:34:53 +0100

    Initial Commit

---
+++ lobo1/lobo1App.swift
@@ -0,0 +1,17 @@
+//
+//  lobo1App.swift
+//  lobo1
+//
+//  Created by alumno on 16/1/25.
+//
+
+import SwiftUI
+
+@main
+struct lobo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
